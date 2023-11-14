39222a97fa92c68bf56bb0cc0f5050b6860a5fb8
Viet Cao <viet.ch2612@gmail.com>
2023-11-15 02:00:36 +0700

    Dummy code for a basic flow

--- src/app.js
+++ src/app.js
@@ -25,6 +25,62 @@ app.io = io;
 // Use trip routes
 app.use('/api/trip', tripRoutes);
 
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Passenger start a trip
+  socket.on('new_trip', (data) => {
+    const trip = JSON.parse(data)
+
+    // Join the specified room
+    socket.join(trip.tripId);
+    console.log(`New trip: ${trip.tripId}`);
+
+    // Finding driver
+    io.to(trip.tripId).emit('finding_driver', 'finding_driver');
+    console.log(`Finding driver: ${trip.tripId}`);
+  });
+
+  socket.on('accept_trip', (data) => {
+    const trip = JSON.parse(data)
+    console.log(`Driver ${trip.driverName} joined: ${trip.tripId}`);
+
+    // Finding driver
+    io.to(trip.tripId).emit('picking_up', trip);
+    console.log(`Driver accepted: ${trip.tripId}`);
+  });
+
+  socket.on('location_update', (data) => {
+    const trip = JSON.parse(data)
+    console.log(`Driver ${trip.driverName} updated location`);
+
+    // Finding driver
+    io.to(trip.tripId).emit('location_update', trip);
+    console.log(`Driver updated location: ${trip.tripId}`);
+  });
+
+  socket.on('in_transit', (data) => {
+    const trip = JSON.parse(data)
+    console.log(`In transit ${trip.tripId}`);
+
+    io.to(trip.tripId).emit('in_transit', trip);
+    console.log(`in_transit: ${trip.tripId}`);
+  });
+
+  socket.on('completed', (data) => {
+    const trip = JSON.parse(data)
+    console.log(`Trip ${trip.tripId} is completed!`);
+
+    io.to(trip.tripId).emit('completed', trip);
+  });
+
+  // Handling disconnection
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+
 // Sync database and start the server
 sequelize.sync().then(() => {
   console.log('PostgreSQL database connected');
@@ -33,13 +89,3 @@ sequelize.sync().then(() => {
   });
 });
 
-// Socket.IO logic for real-time updates
-io.on('connection', (socket) => {
-  console.log('A user connected');
-
-  // You can implement more Socket.IO logic here
-  // For example, handle disconnections, listen for specific events, etc.
-
-  // Example: Emit a welcome message to the connected user
-  socket.emit('message', 'Welcome to the server!');
-});
